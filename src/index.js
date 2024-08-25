9537f8fad7ddd0d4ed66f5152b2b785ae665c3a1
Rahul <rahulsah7846@gmail.com>
2024-08-26 03:02:10 +0530

    updation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 // require('dotenv').config({path:'../env'});
 import 'dotenv/config';
 import connectDB from "./db/index.js";
-connectDB();
+import app from './app.js';
+const port = process.env.PORT || 8080;
+connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log(`ERRR : `, error);
+            throw error;
+        })
+        app.listen(port, () => {
+            console.log(`App is listening at port ${port}`);
+        })
+    })
+    .catch((error) => {
+        console.log(`Error in database connection`, error);
+    })
